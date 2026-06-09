8880c97ee9daaeb0ba353a773284985f0ba62256
Educa0198 <vieiraeduardo2006@gmail.com>
2026-06-09 16:31:36 -0300

    UPDATE

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const env_1 = require("./config/env");
-const logger_1 = require("./utils/logger");
-const app = (0, app_1.createApp)();
-app.listen(env_1.env.PORT, () => {
-    logger_1.logger.info("Server started", { port: env_1.env.PORT, env: env_1.env.NODE_ENV });
-});
